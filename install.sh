2ae2d21a20a2a8269bf8736151e69c743a7add74
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-04-01 17:59:14 +0000

    turns out it was jus thte git clone command that needed the sudo

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-sudo cd /usr/local/src
+cd /usr/local/src
 # In case an older version of confy has already been installed
-sudo rm -rf confy
-git clone https://www.github.com/mikeandike523/confy
-sudo cd confy
-chmod +x ./configure
-./configure
+rm -rf confy
+sudo git clone https://www.github.com/mikeandike523/confy
+cd confy
+sudo chmod +x ./configure
+sudo ./configure
